de72d1a88662f1b1ff698071524880dc550f00a8
jialiya <jialiya.huang@students.olin.edu>
2013-03-06 14:07:43 -0500

    added fb login

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     http = require('http'),
     path = require('path'),
-    lifegraph = require('lifegraph');
+    lifegraph = require('lifegraph'),
+    rem = require('rem');
 
 var lifegraph_serial = require('./controllers/lifegraph_serial');
 
@@ -30,12 +31,58 @@ app.configure(function () {
   app.use('/', express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function () {
-  app.set('host', 'localhost:' + app.get('port'));
-  app.use(express.logger('dev'));
-  app.use(express.errorHandler());
+var fb = rem.connect('facebook.com', '1.0').configure({
+  key: process.env.FB_KEY,
+  secret: process.env.FB_SECRET
 });
 
+// Crudely store user access tokens in a global hash for the duration of
+// the Heroku app's life.
+//
+// In production, you would probably replace these with database-backed
+// functions. As a convenience, these are written as asynchronous functions,
+// though that's totally superfluous here.
+
+var keys = {}
+  , keyskey = 'im not actually a beggar, im actually a... magic man';
+
+function hashUserId (id) {
+  return require('crypto').createHmac('sha1', keyskey).update(id).digest('hex');
+}
+
+function storeCredentials (id, state, next) {
+  keys[hashUserId(id)] = state;
+  next(null);
+}
+
+function restoreCredentials (hash, next) {
+  next(null, keys[hash]);
+}
+
+function clearStoredCredentials (id, next) {
+  next(!(delete keys[hashUserId(id)]));
+}
+
+// The oauth middleware intercepts the callback url that we set when we
+// created the oauth middleware.
+var oauth = rem.oauth(fb, 'http://' + app.get('host') + '/oauth/callback/');
+app.use(oauth.middleware(function (req, res, next) {
+  console.log("User is now authenticated.");
+  var user = oauth.session(req);
+  user('me').get(function (err, json) {
+    user.saveState(function (state) {
+      if (err || !json.id) {
+        res.redirect('/error');
+      }
+
+      storeCredentials(json.id, state, function () {
+        res.redirect('/');
+      });
+    })
+  });
+}));
+
+// connect lifegraph serial port
 lifegraph_serial.setPidCallback(arduino_port, function(pid) {
   lifegraph.connect(pid, function (error, facebookUser) {
 
@@ -52,9 +99,34 @@ lifegraph_serial.setPidCallback(arduino_port, function(pid) {
  * Routes
  */
 
+// Login route calls oauth.startSession, which redirects to an oauth URL.
+app.get('/login/', oauth.login({
+  scope: ['publish_stream']
+}));
+
+// Logout route clears the user's session.
+// Use middleware to clear the tokens from our tokens store as well.
+app.get('/logout/', function (req, res, next) {
+  var user = oauth.session(req);
+  if (!user) {
+    return next();
+  }
+
+  user('me').get(function (err, json) {
+    if (json && json.id) {
+      clearStoredCredentials(json.id, next);
+    } else {
+      next();
+    }
+  })
+}, oauth.logout(function (req, res) {
+  res.redirect('/');
+}));
+
 app.get('/', function(req, res) {
   res.render('index.jade');
 });
+
 
 /**
  * Launch.
